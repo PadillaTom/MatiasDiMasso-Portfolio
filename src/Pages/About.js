b6ec09158ff0d9db49defb5ee03b4d5965ce64f3
Tomas Padilla <padillatomasagustin@gmail.com>
2021-05-29 17:11:02 +0200

    About OK

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
+import ProfilePicture from "../Assets/Portfolio headshot edited.jpg"
+
 const About = () => {
     return (
-        <div>
-            <h1>About Me</h1>
-        </div>
+        <section className="section about-sect">
+            <div className="about-img-container">
+                <img src={ProfilePicture} alt="Matias Di Maso Profile" />
+            </div>
+
+            <div className="about-text-container">
+                <p>
+                Born and raised in Buenos Aires,
+                shaped and crafted in London.
+                Currently working at Strike UX
+                as a digital designer and social
+                media manager.
+                </p>
+            </div>
+            <div className="page-title">
+                About
+            </div>
+        </section>
     )
 }
 
