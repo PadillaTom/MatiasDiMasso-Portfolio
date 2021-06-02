1eaabe51f2da824c0534a533bccc91d5158d5cf9
Tomas Padilla <padillatomasagustin@gmail.com>
2021-06-02 12:22:23 +0200

    Works

--- src/Utils/MyWorks.js
+++ src/Utils/MyWorks.js
@@ -10,5 +10,24 @@ const MyWorks = [
         subTitle: "SubTitulo",
         client: "Cliente",
         location: "Location",
-    }
+        responsabilities: "Responsabilities",
+        tools: [
+            "Figma",
+            "Adobe XD",
+            "Photoshop",
+        ],
+        images: [
+            "Imagen 1",
+            "Imagen 2",
+            "Imagen 3",
+        ],        
+    },
+    {
+        id: 2,
+        imageUrl: "ImageUrl",
+        title: "Titulo",
+        subTitle: "SubTitulo",
+        client: "Cliente",
+        location: "Location",
+    },
 ]
